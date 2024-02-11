692b7175b60d7361afcb2384cbeafd5445dd7e88
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-11 20:09:32 -0300

    feat: create an http-helper and class error

--- src/controllers/tasks/task-controller.spec.ts
+++ src/controllers/tasks/task-controller.spec.ts
@@ -1,3 +1,5 @@
+import { MissingParamError } from "../../utils/errors/missing-param-error"
+import { badRequest } from "../../utils/helpers/http-helper"
 import { TaskController } from "./task-controller"
 
 interface SutTypes {
@@ -15,6 +17,6 @@ describe('Task Controller', () => {
   test('should return 400 if message is not provided', async () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({ body: {} })
-    expect(httpResponse).toEqual({ statusCode: 400, body: new Error('Missing Param Error')})
+    expect(httpResponse).toEqual(badRequest(new MissingParamError('Message')))
   })
 })
--- src/controllers/tasks/task-controller.ts
+++ src/controllers/tasks/task-controller.ts
@@ -1,9 +1,11 @@
-import { Controller, HttpRequest, HttpResponse } from "../protocols";
+import { MissingParamError } from "../../utils/errors/missing-param-error";
+import { badRequest } from "../../utils/helpers/http-helper";
+import { Controller, HttpRequest, HttpResponse } from "../../utils/protocols";
 
 export class TaskController implements Controller {
   async handle (httpRequest: HttpRequest):Promise<HttpResponse> {
     const { message } = httpRequest.body
-    if (!message) return { statusCode: 400, body: new Error('Missing Param Error')}
+    if (!message) return badRequest(new MissingParamError('Message'))
     return { statusCode: 0, body: {} }
   }
 }
