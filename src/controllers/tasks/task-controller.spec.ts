9608717f1ce7498df211d80e08cafbe59731ca76
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-11 19:56:21 -0300

    refactor: move sut creation to a factory method

--- src/controllers/tasks/task-controller.spec.ts
+++ src/controllers/tasks/task-controller.spec.ts
@@ -1,8 +1,19 @@
 import { TaskController } from "./task-controller"
 
+interface SutTypes {
+  sut: TaskController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new TaskController()
+  return {
+    sut
+  }
+}
+
 describe('Task Controller', () => {
   test('should return 400 if message is not provided', async () => {
-    const sut = new TaskController()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({ body: {} })
     expect(httpResponse).toEqual({ statusCode: 400, body: new Error('Missing Param Error')})
   })
