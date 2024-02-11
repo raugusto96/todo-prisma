f1cf544f9941cfb1df03101a1ffda2e6cfaf5484
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-11 20:14:21 -0300

    feat: ensure TaskController return 400 if status is not provided

--- src/controllers/tasks/task-controller.ts
+++ src/controllers/tasks/task-controller.ts
@@ -4,8 +4,9 @@ import { Controller, HttpRequest, HttpResponse } from "../../utils/protocols";
 
 export class TaskController implements Controller {
   async handle (httpRequest: HttpRequest):Promise<HttpResponse> {
-    const { message } = httpRequest.body
+    const { message, status } = httpRequest.body
     if (!message) return badRequest(new MissingParamError('Message'))
+    if (!status) return badRequest(new MissingParamError('Status'))
     return { statusCode: 0, body: {} }
   }
 }
