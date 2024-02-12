c182656da45236d6a956e9d54394c82611168be4
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-12 07:51:02 -0300

    feat: ensure TaskController returns an task on success

--- src/controllers/tasks/task-controller.ts
+++ src/controllers/tasks/task-controller.ts
@@ -1,24 +1,24 @@
-import { Controller, HttpRequest, HttpResponse } from "../../utils/protocols";
-import { badRequest, serverError } from "../../utils/helpers/http-helper";
-import { MissingParamError } from "../../utils/errors/missing-param-error";
-import { CreateTaskService } from "../../services/tasks/protocols/add-task-service";
+import { Controller, HttpRequest, HttpResponse } from '../../utils/protocols'
+import { badRequest, serverError } from '../../utils/helpers/http-helper'
+import { MissingParamError } from '../../utils/errors/missing-param-error'
+import { CreateTaskService } from '../../services/tasks/protocols/add-task-service'
 
 export class TaskController implements Controller {
   constructor(private readonly addTaskService: CreateTaskService) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ["message", "status"];
+      const requiredFields = ['message', 'status']
       for (const field of requiredFields) {
         if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field));
+          return badRequest(new MissingParamError(field))
         }
       }
-      const { message, status } = httpRequest.body;
-      await this.addTaskService.add({ message, status });
-      return { statusCode: 0, body: {} };
+      const { message, status } = httpRequest.body
+      const task = await this.addTaskService.add({ message, status })
+      return { statusCode: 200, body: task }
     } catch (error) {
-      return serverError(error);
+      return serverError(error)
     }
   }
 }
