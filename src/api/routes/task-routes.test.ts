d74a78b0711b4d6b02036d8b4f34bfce181fbfbe
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-12 13:14:49 -0300

    test: ensure POST /task return 200 on add task sucess

---
+++ src/api/routes/task-routes.test.ts
@@ -0,0 +1,34 @@
+import request from 'supertest'
+import app from '../config/app'
+import { MongoHelper } from '../utils/helpers/mongo-helper'
+import { type Collection } from 'mongodb'
+import env from '../config/env'
+
+let taskCollection: Collection
+
+describe('Task Routes', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl ?? '')
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    taskCollection = await MongoHelper.getCollection('tasks')
+    await taskCollection.deleteMany({})
+  })
+
+  describe('POST /task', () => {
+    test('should return 200 on add task success', async () => {
+      await request(app)
+        .post('/api/task')
+        .send({
+          message: 'Task',
+          status: 'pending'
+        })
+        .expect(200)
+    })
+  })
+})
