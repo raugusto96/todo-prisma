a33a0b58fe4c483842e61910514a1f9fb598280b
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-13 12:00:00 -0300

    test: add LoadTaskController

---
+++ src/api/controllers/tasks/load-tasks-controller.spec.ts
@@ -0,0 +1,64 @@
+import { Task } from '@prisma/client'
+import { ok } from '../../utils/helpers/http-helper'
+import { LoadDbTasksRepository } from '../../repositories/db/usecases/load-tasks'
+import { LoadTasksController } from './load-tasks-controller'
+
+const makeLoadTaskRepositoryStub = () => {
+  class LoadTasksRepository implements LoadDbTasksRepository {
+    async load(): Promise<Task[]> {
+      return Promise.resolve([
+        {
+          id: 'any_valid_id',
+          message: 'any_valid_message',
+          status: 'any_valid_status'
+        }
+      ])
+    }
+  }
+  return new LoadTasksRepository()
+}
+
+interface SutTypes {
+  sut: LoadTasksController
+  loadTasksRepository: LoadDbTasksRepository
+}
+
+const makeSut = (): SutTypes => {
+  const loadTasksRepository = makeLoadTaskRepositoryStub()
+  const sut = new LoadTasksController(loadTasksRepository)
+  return {
+    sut,
+    loadTasksRepository
+  }
+}
+
+const makeFakeTask = (): Task[] => [
+  {
+    id: 'any_valid_id',
+    message: 'any_valid_message',
+    status: 'any_valid_status'
+  }
+]
+
+describe('LoadTaskController', () => {
+  test('should return an empty array if LoadTasksRepository not find an task', async () => {
+    const { sut, loadTasksRepository } = makeSut()
+    jest
+      .spyOn(loadTasksRepository, 'load')
+      .mockReturnValueOnce(Promise.resolve([]))
+    const httpRequest = {
+      body: {}
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(ok([]))
+  })
+
+  test('should returns an task array on success', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {}
+    }
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(ok(makeFakeTask()))
+  })
+})
