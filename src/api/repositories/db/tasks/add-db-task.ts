7fc2671203e2587c90110234b209de9a554916d5
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-13 11:00:26 -0300

    refactor: change returned Task for AddDbTaskRepository with Task in Prisma/client

--- src/api/repositories/db/tasks/add-db-task.ts
+++ src/api/repositories/db/tasks/add-db-task.ts
@@ -1,6 +1,6 @@
-import { Prisma } from '@prisma/client'
+import { Prisma, Task } from '@prisma/client'
 import prisma from '../../../config/prismaClient'
-import { STATUS, Task } from '../../../models/usecases'
+import { STATUS } from '../../../models/usecases'
 import { AddDbTaskRepository } from '../usecases/add-task'
 
 export class AddTaskDbRepository implements AddDbTaskRepository {
