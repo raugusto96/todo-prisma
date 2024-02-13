50a9ac74a7c89673109ab427af6ac595fea3cca6
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-13 09:36:54 -0300

    feat: ensure LoadTaskController calls LoadTaskRepository with correct values

--- src/api/controllers/tasks/load-task-controller.ts
+++ src/api/controllers/tasks/load-task-controller.ts
@@ -1,16 +1,19 @@
+import { LoadDbTaskRepository } from '../../repositories/db/usecases/load-task'
 import { MissingParamError } from '../../utils/errors'
 import { badRequest } from '../../utils/helpers/http-helper'
 import { Controller, HttpResponse } from '../../utils/protocols'
 
 export class LoadTaskController implements Controller {
+  constructor(private readonly loadDbTaskRepository: LoadDbTaskRepository) {}
+
   async handle(request: LoadTaskController.Request): Promise<HttpResponse> {
     const requiredFields = ['taskId']
     for (const field of requiredFields) {
-      console.log(request[field])
       if (!request[field]) {
         return badRequest(new MissingParamError(field))
       }
     }
+    await this.loadDbTaskRepository.load(request.taskId)
     return {
       statusCode: 0,
       body: ''
