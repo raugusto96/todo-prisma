432b391fce09d8495e4909bc7e3677c3862abc1c
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-13 14:23:18 -0300

    feat: add put route to update task

--- src/api/routes/task-routes.ts
+++ src/api/routes/task-routes.ts
@@ -3,9 +3,11 @@ import { adaptRoute } from '../utils/adapters/express-route-adapter'
 import { makeAddTaskController } from '../utils/factories/controllers/tasks/add-task-controller-factory'
 import { makeLoadTaskController } from '../utils/factories/controllers/tasks/load-task-controller-factory'
 import { makeLoadTasksController } from '../utils/factories/controllers/tasks/load-tasks-controller-factory'
+import { makeUpdateTasksController } from '../utils/factories/controllers/tasks/update-tasks-controller-factory'
 
 export default (router: Router): void => {
   router.post('/task', adaptRoute(makeAddTaskController()))
   router.get('/task', adaptRoute(makeLoadTasksController()))
   router.get('/task/:taskId', adaptRoute(makeLoadTaskController()))
+  router.put('/task/:taskId', adaptRoute(makeUpdateTasksController()))
 }
