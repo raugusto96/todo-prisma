de496fd320654005a42e7b005c54a1d83ca4a0d6
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-13 09:11:03 -0300

    refactor: move sut creation to a factory method

--- src/api/controllers/tasks/load-task-controller.spec.ts
+++ src/api/controllers/tasks/load-task-controller.spec.ts
@@ -2,9 +2,20 @@ import { MissingParamError } from '../../utils/errors'
 import { badRequest } from '../../utils/helpers/http-helper'
 import { LoadTaskController } from './load-task-controller'
 
+interface SutTypes {
+  sut: LoadTaskController
+}
+
+const makeSut = (): SutTypes => {
+  const sut = new LoadTaskController()
+  return {
+    sut
+  }
+}
+
 describe('LoadTaskController', () => {
   test('should return 400 if taskId is not provided', async () => {
-    const sut = new LoadTaskController()
+    const { sut } = makeSut()
     const httpResponse = await sut.handle({ taskId: '' })
     expect(httpResponse).toEqual(badRequest(new MissingParamError('taskId')))
   })
