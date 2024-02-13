7d460b159da697782ba4bc3d10795fef450610eb
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-13 12:32:12 -0300

    refactor: move sut creation to a factory method

--- src/api/repositories/db/tasks/update-db-task.spec.ts
+++ src/api/repositories/db/tasks/update-db-task.spec.ts
@@ -2,19 +2,32 @@ import { Task } from '@prisma/client'
 import { LoadDbTaskRepository } from '../usecases/load-task'
 import { UpdateTaskDbRepository } from './update-db-task'
 
+interface SutTypes {
+  sut: UpdateTaskDbRepository
+  loadTaskDbRepositoryStub: LoadDbTaskRepository
+}
+
+const makeSut = (): SutTypes => {
+  class LoadTaskDbRepository implements LoadDbTaskRepository {
+    async load(taskId: string): Promise<Task> {
+      return Promise.resolve({
+        id: 'any_valid_id',
+        message: 'any_valid_message',
+        status: 'any_valid_status'
+      })
+    }
+  }
+  const loadTaskDbRepositoryStub = new LoadTaskDbRepository()
+  const sut = new UpdateTaskDbRepository(loadTaskDbRepositoryStub)
+  return {
+    sut,
+    loadTaskDbRepositoryStub
+  }
+}
+
 describe('UpdateDbTaskRepository', () => {
   test('should calls LoadDbTaskRepository with correct value', async () => {
-    class LoadTaskDbRepository implements LoadDbTaskRepository {
-      async load(taskId: string): Promise<Task> {
-        return Promise.resolve({
-          id: 'any_valid_id',
-          message: 'any_valid_message',
-          status: 'any_valid_status'
-        })
-      }
-    }
-    const loadTaskDbRepositoryStub = new LoadTaskDbRepository()
-    const sut = new UpdateTaskDbRepository(loadTaskDbRepositoryStub)
+    const { sut, loadTaskDbRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadTaskDbRepositoryStub, 'load')
     await sut.update('any_valid_id')
     expect(loadSpy).toHaveBeenCalledWith('any_valid_id')
