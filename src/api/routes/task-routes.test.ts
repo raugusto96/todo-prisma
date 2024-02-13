58d87f393e171e5d18c730626baf5f0e928e2554
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-13 16:53:27 -0300

    test: add route to delete a task

--- src/api/routes/task-routes.test.ts
+++ src/api/routes/task-routes.test.ts
@@ -3,6 +3,7 @@ import app from '../config/app'
 import { MongoHelper } from '../utils/helpers/mongo-helper'
 import { type Collection } from 'mongodb'
 import env from '../config/env'
+import { STATUS } from '../models/enums'
 
 let taskCollection: Collection
 
@@ -28,6 +29,72 @@ describe('Task Routes', () => {
           message: 'Task'
         })
         .expect(200)
+    })
+  })
+
+  describe('GET /task', () => {
+    test('should return 204 on get tasks if do not have tasks registered', async () => {
+      await request(app).get('/api/task').expect(204)
+    })
+
+    test('should return 200 on get tasks on success', async () => {
+      await request(app)
+        .post('/api/task')
+        .send({
+          message: 'Task'
+        })
+        .expect(200)
+      await request(app).get('/api/task').expect(200)
+    })
+  })
+
+  describe('GET /task/:taskId', () => {
+    test('should return 200 on get tasks on success', async () => {
+      const task = await taskCollection.insertOne({
+        message: 'Task',
+        status: STATUS.PENDING
+      })
+      await request(app)
+        .get(`/api/task/${task.insertedId.toString()}`)
+        .expect(200)
+    })
+
+    test('should return 404 if not found a task', async () => {
+      await request(app).get(`/api/task/65cba4decf11db8558825326`).expect(404)
+    })
+  })
+
+  describe('PUT /task/:taskId', () => {
+    test('should return 200 when update a task on success', async () => {
+      const task = await taskCollection.insertOne({
+        message: 'Task',
+        status: STATUS.PENDING
+      })
+      await request(app)
+        .put(`/api/task/${task.insertedId.toString()}`)
+        .expect(200)
+    })
+
+    test('should return 404 if not found a task', async () => {
+      await request(app).put(`/api/task/65cba4decf11db8558825326`).expect(404)
+    })
+  })
+
+  describe('DELETE /task/:taskId', () => {
+    test('should return 204 when delete a task on success', async () => {
+      const task = await taskCollection.insertOne({
+        message: 'Task',
+        status: STATUS.PENDING
+      })
+      await request(app)
+        .delete(`/api/task/${task.insertedId.toString()}`)
+        .expect(204)
+    })
+
+    test('should return 404 if not found a task', async () => {
+      await request(app)
+        .delete(`/api/task/65cba4decf11db8558825326`)
+        .expect(404)
     })
   })
 })
