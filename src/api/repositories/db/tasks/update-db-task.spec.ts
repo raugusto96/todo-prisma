ab9d91b653f18bceda732a870f6c0e3b340e7ce5
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-13 13:05:21 -0300

    test: ensure UpdateDbTaskRepository returns an updated task on success

--- src/api/repositories/db/tasks/update-db-task.spec.ts
+++ src/api/repositories/db/tasks/update-db-task.spec.ts
@@ -1,6 +1,10 @@
 import { Task } from '@prisma/client'
 import { LoadDbTaskRepository } from '../usecases/load-task'
 import { UpdateTaskDbRepository } from './update-db-task'
+import { UpdateTaskDTO } from '../../../models/dtos'
+import { MongoHelper } from '../../../utils/helpers/mongo-helper'
+import env from '../../../config/env'
+import { Collection, ObjectId } from 'mongodb'
 
 const makeLoadTaskDbRepositoryStub = () => {
   class LoadTaskDbRepository implements LoadDbTaskRepository {
@@ -29,20 +33,74 @@ const makeSut = (): SutTypes => {
   }
 }
 
+let taskCollection: Collection
+
 describe('UpdateDbTaskRepository', () => {
+  beforeAll(async () => {
+    await MongoHelper.connect(env.mongoUrl)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    taskCollection = await MongoHelper.getCollection('Task')
+    await taskCollection.deleteMany({})
+  })
+
   test('should calls LoadDbTaskRepository with correct value', async () => {
     const { sut, loadTaskDbRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadTaskDbRepositoryStub, 'load')
-    await sut.update('any_valid_id')
-    expect(loadSpy).toHaveBeenCalledWith('any_valid_id')
+    const task = await taskCollection.insertOne({
+      message: 'any_message',
+      status: 'any_status'
+    })
+    const data: UpdateTaskDTO = {
+      message: 'any_updated_message',
+      status: 'any_updated_message'
+    }
+    await sut.update(task.insertedId.toString(), data)
+    expect(loadSpy).toHaveBeenCalledWith(task.insertedId.toString())
   })
 
   test('should returns null if LoadDbTaskRepository not found an task with id', async () => {
     const { sut, loadTaskDbRepositoryStub } = makeSut()
+    const data: UpdateTaskDTO = {
+      message: 'any_updated_message',
+      status: 'any_updated_message'
+    }
     jest
       .spyOn(loadTaskDbRepositoryStub, 'load')
       .mockReturnValueOnce(Promise.resolve(null))
-    const task = await sut.update('any_valid_id')
+    const task = await sut.update('any_valid_id', data)
     expect(task).toBeNull()
+  })
+
+  test('should returns an updated task on success', async () => {
+    const { sut } = makeSut()
+    const data: UpdateTaskDTO = {
+      message: 'any_updated_message',
+      status: 'any_updated_status'
+    }
+    const task = await taskCollection.insertOne({
+      message: 'any_message',
+      status: 'any_status'
+    })
+    const objectId = new ObjectId(task.insertedId)
+    await taskCollection.findOneAndUpdate(
+      {
+        _id: objectId
+      },
+      {
+        $set: { message: data.message, status: data.status }
+      }
+    )
+    const updatedTask = await sut.update(task.insertedId.toString(), data)
+    expect(updatedTask).toEqual({
+      id: task.insertedId.toString(),
+      message: 'any_updated_message',
+      status: 'any_updated_status'
+    })
   })
 })
