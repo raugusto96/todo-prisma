48eff5487736fae3f49becda511eaf518edcf6d2
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-13 09:37:11 -0300

    test: ensure LoadTaskController calls LoadTaskRepository with correct values

--- src/api/controllers/tasks/load-task-controller.spec.ts
+++ src/api/controllers/tasks/load-task-controller.spec.ts
@@ -1,15 +1,33 @@
+import { Task } from '@prisma/client'
 import { MissingParamError } from '../../utils/errors'
 import { badRequest } from '../../utils/helpers/http-helper'
 import { LoadTaskController } from './load-task-controller'
+import { LoadDbTaskRepository } from '../../repositories/db/usecases/load-task'
+
+const makeLoadTaskRepositoryStub = () => {
+  class LoadTaskRepository implements LoadDbTaskRepository {
+    async load(taskId: string): Promise<Task> {
+      return Promise.resolve({
+        id: 'any_valid_id',
+        message: 'any_valid_message',
+        status: 'any_valid_status'
+      })
+    }
+  }
+  return new LoadTaskRepository()
+}
 
 interface SutTypes {
   sut: LoadTaskController
+  loadTaskRepository: LoadDbTaskRepository
 }
 
 const makeSut = (): SutTypes => {
-  const sut = new LoadTaskController()
+  const loadTaskRepository = makeLoadTaskRepositoryStub()
+  const sut = new LoadTaskController(loadTaskRepository)
   return {
-    sut
+    sut,
+    loadTaskRepository
   }
 }
 
@@ -18,5 +36,15 @@ describe('LoadTaskController', () => {
     const { sut } = makeSut()
     const httpResponse = await sut.handle({ taskId: '' })
     expect(httpResponse).toEqual(badRequest(new MissingParamError('taskId')))
+  })
+
+  test('should call LoadTaskRepository with correct values', async () => {
+    const { sut, loadTaskRepository } = makeSut()
+    const loadSpy = jest.spyOn(loadTaskRepository, 'load')
+    const request = {
+      taskId: 'any_valid_id'
+    }
+    await sut.handle(request)
+    expect(loadSpy).toHaveBeenCalledWith('any_valid_id')
   })
 })
