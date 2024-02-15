5064e1849da0acb94b9de0b381382262c702fd39
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-15 09:09:41 -0300

    refactor: change import routes

--- api/src/config/routes.ts
+++ api/src/config/routes.ts
@@ -1,17 +1,8 @@
 import { type Express, Router } from 'express'
-import { readdirSync } from 'fs'
-import path from 'path'
+import taskRoutes from '../routes/task-routes'
 
 export default (app: Express): void => {
   const router = Router()
   app.use('/api', router)
-  readdirSync(path.resolve(__dirname, '../routes')).map(async (file) => {
-    if (!file.includes('.test.')) {
-      if (await import(`../routes/${file}`)) {
-        ;(await import(`../routes/${file}`)).default(router)
-      } else {
-        console.warn(`Warning: '${file}' does not export a default function.`)
-      }
-    }
-  })
+  taskRoutes(router)
 }
