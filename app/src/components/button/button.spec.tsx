9a1dbc5662dc9bab6805bcc1007d7fecd93917c8
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-17 09:17:28 -0300

    test: ensure Button returns a displayValue props correctely

--- app/src/components/button/button.spec.tsx
+++ app/src/components/button/button.spec.tsx
@@ -3,15 +3,22 @@ import { Button } from "./Button"
 
 describe('Button Component', () => {
   test('should render the component', () => {
-    render(<Button />)
+    const displayValue = 'any_value'
+    render(<Button
+      displayValue={displayValue}
+    />)
     const button = screen.getByRole('button')
 
     expect(button).toBeInTheDocument()
   })
 
-  // test('should render returns a button element', () => {
-  //   const { container } = render(<Button />)
+  test('should render returns a button element with correct display value is provided', () => {
+    const displayValue = 'any_value'
+    render(<Button
+      displayValue={displayValue}
+    />)
+    const button = screen.getByText('any_value')
 
-  //   expect(container).toBe
-  // })
+    expect(button).toBeInTheDocument()
+  })
 })
