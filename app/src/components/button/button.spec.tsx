a0f0212276d4f4a3ee05f5af33ca4e608d1bb3d7
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-17 09:22:26 -0300

    refactor: move sut creation to a factory method

--- app/src/components/button/button.spec.tsx
+++ app/src/components/button/button.spec.tsx
@@ -1,22 +1,29 @@
 import { render, screen } from "../../utils/test/test-utils"
 import { Button } from "./Button"
+import { ButtonProps } from "./protocols/button"
+
+const makeSut = () => {
+  const props: ButtonProps = {
+    displayValue: 'any_value'
+  }
+  
+  return (
+    <Button {...props} />
+  )
+}
 
 describe('Button Component', () => {
   test('should render the component', () => {
-    const displayValue = 'any_value'
-    render(<Button
-      displayValue={displayValue}
-    />)
+    const sut = makeSut()
+    render(sut)
     const button = screen.getByRole('button')
 
     expect(button).toBeInTheDocument()
   })
 
   test('should render returns a button element with correct display value is provided', () => {
-    const displayValue = 'any_value'
-    render(<Button
-      displayValue={displayValue}
-    />)
+    const sut = makeSut()
+    render(sut)
     const button = screen.getByText('any_value')
 
     expect(button).toBeInTheDocument()
