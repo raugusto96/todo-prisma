fd2c0d092873fc0c0ceb5214b3394c6e215d16a4
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-19 16:17:16 -0300

    test: ensure RemoteTask calls HttpPostClient with correct data

--- app/src/utils/api/usecases/task/remote-task.spec.ts
+++ app/src/utils/api/usecases/task/remote-task.spec.ts
@@ -22,7 +22,17 @@ describe("RemoteTask", () => {
   test("should call HttpPostClient with the correct url", async () => {
     const url = faker.internet.url();
     const { sut, httpPostClientSpy } = makeSut(url);
-    await sut.task();
+    await sut.task(faker.commerce.productName());
     expect(httpPostClientSpy.url).toBe(url);
+  });
+
+  test("should call HttpPostClient with the correct data", async () => {
+    const message = faker.commerce.productName();
+    const data = {
+      message,
+    };
+    const { sut, httpPostClientSpy } = makeSut();
+    await sut.task(message);
+    expect(httpPostClientSpy.data).toEqual(data);
   });
 });
