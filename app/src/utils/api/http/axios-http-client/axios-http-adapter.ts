7341961c5540d904a0108436e17b60a399515b93
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-20 20:04:33 -0300

    feat: ensure AxiosHttpAdapter returns correct values

---
+++ app/src/utils/api/http/axios-http-client/axios-http-adapter.ts
@@ -0,0 +1,16 @@
+import {
+  HttpPostClient,
+  HttpPostParams,
+  HttpResponse,
+} from "../../protocols/http";
+import axios from "axios";
+
+export class AxiosHttpAdapter implements HttpPostClient<any, any> {
+  async post(params: HttpPostParams<any>): Promise<HttpResponse<any>> {
+    const httpResponse = await axios.post(params.url, params.body);
+    return {
+      statusCode: httpResponse.status,
+      body: httpResponse.data,
+    };
+  }
+}
