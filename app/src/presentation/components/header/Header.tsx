40555d8619411eb3f9ccb28805e38695d1a69392
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-21 22:54:07 -0300

    feat: add header component

---
+++ app/src/presentation/components/header/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Header as Head } from "./Header.styles";
+
+const Header: React.FC = () => {
+  return (
+    <Head>
+      <h1>ToDo Prisma</h1>
+    </Head>
+  );
+};
+
+export default Header;
