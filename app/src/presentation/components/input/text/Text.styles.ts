d62fe3a5e53fb4f7081b789316f58606dc76f621
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-22 07:51:26 -0300

    feat: add input style

--- app/src/presentation/components/input/text/Text.styles.ts
+++ app/src/presentation/components/input/text/Text.styles.ts
@@ -2,4 +2,7 @@ import styled from "styled-components";
 
 export const Label = styled.label``;
 
-export const Input = styled.input``;
+export const Input = styled.input`
+  font-size: 1.2rem;
+  padding: 0.5rem;
+`;
