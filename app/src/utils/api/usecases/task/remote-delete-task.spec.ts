611a0e7f691221b4af185f6820834914ec1e40d5
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-23 19:48:17 -0300

    refactor: move sut creation to a factory method

--- app/src/utils/api/usecases/task/remote-delete-task.spec.ts
+++ app/src/utils/api/usecases/task/remote-delete-task.spec.ts
@@ -4,11 +4,24 @@ import { RemoteDeleteTask } from "./remote-delete-task";
 
 const makeHttpClientSpy = () => new HttpClientSpy();
 
+interface SutTypes {
+  sut: RemoteDeleteTask;
+  httpClientSpy: HttpClientSpy;
+}
+
+const makeSut = (url: string): SutTypes => {
+  const httpClientSpy = makeHttpClientSpy();
+  const sut = new RemoteDeleteTask(url, httpClientSpy);
+  return {
+    sut,
+    httpClientSpy,
+  };
+};
+
 describe("RemoteDeleteTask", () => {
   test("should call HttpClient with the correct url and verb", async () => {
     const url = faker.internet.url();
-    const httpClientSpy = makeHttpClientSpy();
-    const sut = new RemoteDeleteTask(url, httpClientSpy);
+    const { sut, httpClientSpy } = makeSut(url);
     const taskId = faker.database.mongodbObjectId();
     await sut.delete({ taskId });
     expect(httpClientSpy.url).toBe(url);
