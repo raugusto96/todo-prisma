a012395edafe15a19c4b1cadfc26ef70af1ac388
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-23 20:03:52 -0300

    test: ensure RemoteDeleteTask calls HttpClient with correct headers params

--- app/src/utils/api/usecases/task/remote-delete-task.spec.ts
+++ app/src/utils/api/usecases/task/remote-delete-task.spec.ts
@@ -1,5 +1,5 @@
 import { faker } from "@faker-js/faker";
-import { HttpClientSpy } from "../../test/mock";
+import { HttpClientSpy, mockDeleteParams } from "../../test/mock";
 import { RemoteDeleteTask } from "./remote-delete-task";
 
 const makeHttpClientSpy = () => new HttpClientSpy();
@@ -9,7 +9,7 @@ interface SutTypes {
   httpClientSpy: HttpClientSpy;
 }
 
-const makeSut = (url: string): SutTypes => {
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
   const httpClientSpy = makeHttpClientSpy();
   const sut = new RemoteDeleteTask(url, httpClientSpy);
   return {
@@ -22,8 +22,14 @@ describe("RemoteDeleteTask", () => {
   test("should call HttpClient with the correct url and verb", async () => {
     const url = faker.internet.url();
     const { sut, httpClientSpy } = makeSut(url);
-    const taskId = faker.database.mongodbObjectId();
-    await sut.delete({ taskId });
+    await sut.delete(mockDeleteParams());
     expect(httpClientSpy.url).toBe(url);
+  });
+
+  test("should call HttpClient with the headers params", async () => {
+    const params = mockDeleteParams();
+    const { sut, httpClientSpy } = makeSut();
+    await sut.delete(params);
+    expect(httpClientSpy.headers.params).toEqual(params);
   });
 });
