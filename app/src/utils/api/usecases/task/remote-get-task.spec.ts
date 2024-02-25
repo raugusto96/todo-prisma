1bad529a53d2c0ec32733d258c59c532b0148e3c
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-24 21:30:42 -0300

    refactor: move sut creation to a factory method

--- app/src/utils/api/usecases/task/remote-get-task.spec.ts
+++ app/src/utils/api/usecases/task/remote-get-task.spec.ts
@@ -3,11 +3,24 @@ import { faker } from "@faker-js/faker";
 import { GetTask } from "../protocols/get-task";
 import { RemoteGetTask } from "./remote-get-task";
 
+interface SutTypes {
+  sut: RemoteGetTask;
+  httpClientSpy: HttpClientSpy;
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpClientSpy = new HttpClientSpy();
+  const sut = new RemoteGetTask(url, httpClientSpy);
+  return {
+    sut,
+    httpClientSpy,
+  };
+};
+
 describe("RemoteGetTask", () => {
   test("should call HttpClient with correct url and verb", () => {
     const url = faker.internet.url();
-    const httpClientSpy = new HttpClientSpy();
-    const sut = new RemoteGetTask(url, httpClientSpy);
+    const { sut, httpClientSpy } = makeSut(url);
     sut.get({} as GetTask.Params);
     expect(httpClientSpy.url).toBe(url);
   });
