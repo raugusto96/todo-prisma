e96c0fbad8025445789ebf8992e9346adbfe29b3
Rodrigo Augusto <rodrigoaugusto96@outlook.com>
2024-02-24 22:14:04 -0300

    feat: ensure return a task array if HttpClient returns 200

--- app/src/utils/api/usecases/task/remote-get-task.ts
+++ app/src/utils/api/usecases/task/remote-get-task.ts
@@ -2,13 +2,13 @@ import { UnexpectedError } from "../../errors";
 import { HttpClient, HttpStatusCode } from "../../protocols/http";
 import { GetTask } from "../protocols/get-task";
 
-export class RemoteGetTask {
+export class RemoteGetTask implements GetTask {
   constructor(
     private readonly url: string,
     private readonly httpClient: HttpClient
   ) {}
 
-  async get(params: GetTask.Params): Promise<void> {
+  async get(params: GetTask.Params): Promise<GetTask.Model | GetTask.Model[]> {
     const httpResponse = await this.httpClient.request({
       url: this.url,
       method: "get",
